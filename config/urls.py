2e5f71a63b61216e1fe31190227df5d37b7b624e
Dav2dAnderson <dav2danderson@gmail.com>
2026-02-26 06:21:29 +0500

    fixed bug

--- config/urls.py
+++ config/urls.py
@@ -20,7 +20,7 @@ from django.conf import settings
 from django.conf.urls.static import static
 
 # from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
-from user_control.views import CustomTokenObtainPairView
+from users_control.views import CustomTokenObtainPairView
 
 from drf_spectacular.views import SpectacularAPIView, SpectacularRedocView, SpectacularSwaggerView
 
