0f4a355b9ee9ec37de40c8cf730f1b77fd174ee5
Dav2dAnderson <dav2danderson@gmail.com>
2026-03-09 09:26:14 +0500

    changes

--- config/settings.py
+++ config/settings.py
@@ -36,10 +36,6 @@ CORS_ALLOWED_ORIGINS = [
     "https://codylife.uz",
     "https://www.codylife.uz",
     "https://codylife.vercel.app",
-    "http://localhost:3000",
-    "http://127.0.0.1:3000",
-    "http://localhost:3001",
-    "http://127.0.0.1:3001",
 ]
 
 CORS_ALLOW_CREDENTIALS = True
