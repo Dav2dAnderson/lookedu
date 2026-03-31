f096d4b1ea7cc579fe242cd158419eb2d3dd02c3
Dav2dAnderson <dav2danderson@gmail.com>
2026-03-31 21:00:22 +0500

    some fixes

--- config/settings.py
+++ config/settings.py
@@ -174,7 +174,7 @@ EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = "davronbek.n5zarov@gmail.com"
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_HOST = os.getenv("EMAIL_HOST")
-EMAIL_PORT = os.getenv("EMAIL_PORT")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT"))
 EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS")
 
 
