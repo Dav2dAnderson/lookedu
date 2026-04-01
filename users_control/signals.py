bac6cdbd01bc0d4a939653901c151de156ed9e18
Dav2dAnderson <dav2danderson@gmail.com>
2026-04-01 12:11:59 +0500

    changed smtp

--- users_control/signals.py
+++ users_control/signals.py
@@ -1,5 +1,5 @@
 from django.conf import settings
-from django.core.mail import send_mail
+from django.core.mail import send_mail, EmailMessage
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
@@ -10,12 +10,12 @@ from .models import CustomUser
 def send_welcome_email(sender, instance, created, **kwargs):
     if created and instance.email:
         try:
-            send_mail(
-                "Welcome!",
-                "Thanks for registering.",
-                settings.EMAIL_HOST_USER,
-                [instance.email],
-                fail_silently=False
+            email = EmailMessage(
+                subject="Welcome!",
+                body="Thanks for registering.",
+                from_email=settings.EMAIL_HOST_USER,
+                to=[instance.email],
             )
+            email.send(fail_silently=False)
         except Exception as e:
             print(f"Registration email failed to send: {e}")
