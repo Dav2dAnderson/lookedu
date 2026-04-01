aa9fab1495aa00221d529cdb91ff3f102527f872
Dav2dAnderson <dav2danderson@gmail.com>
2026-04-01 12:18:22 +0500

    fix

--- users_control/signals.py
+++ users_control/signals.py
@@ -1,21 +1,26 @@
 from django.conf import settings
-from django.core.mail import send_mail, EmailMessage
 from django.dispatch import receiver
 from django.db.models.signals import post_save
 
 from .models import CustomUser
 
+from django.core.mail import EmailMessage, get_connection
 
 @receiver(post_save, sender=CustomUser)
 def send_welcome_email(sender, instance, created, **kwargs):
     if created and instance.email:
         try:
+            connection = get_connection('django.core.mail.backends.smtp.EmailBackend')
+
             email = EmailMessage(
                 subject="Welcome!",
                 body="Thanks for registering.",
                 from_email=settings.EMAIL_HOST_USER,
                 to=[instance.email],
+                connection=connection
             )
+
             email.send(fail_silently=False)
+
         except Exception as e:
             print(f"Registration email failed to send: {e}")
