c572db3094e3caa287400d84a4847208ab27d933
Dav2dAnderson <dav2danderson@gmail.com>
2026-04-06 20:32:57 +0500

    just for test

--- config/settings.py
+++ config/settings.py
@@ -172,10 +172,10 @@ USE_TZ = True
 """ Email settings """
 EMAIL_BACKEND = 'django.core.mail.backends.smtp.EmailBackend'
 EMAIL_HOST_USER = "davronbek.n5zarov@gmail.com"
-EMAIL_HOST_PASSWORD = "dypygtmddxfltgpi"
-EMAIL_HOST = "smtp.gmail.com"
-EMAIL_PORT = 587
-EMAIL_USE_TLS = True
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+EMAIL_HOST = os.getenv("EMAIL_HOST")
+EMAIL_PORT = int(os.getenv("EMAIL_PORT", 587))
+EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", "True") == "True"
 
 
 STATIC_URL = 'static/'
